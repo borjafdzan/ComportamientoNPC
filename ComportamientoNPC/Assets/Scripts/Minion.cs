5d32c2a1fa6b690c0fd68869d3c9ca1f8eb0d616
borjafdzan <borjafdzan@gmail.com>
2022-05-19 15:19:58 +0200

    Deteccion mediante radio

--- ComportamientoNPC/Assets/Scripts/Minion.cs
+++ ComportamientoNPC/Assets/Scripts/Minion.cs
@@ -6,19 +6,46 @@ using UnityEngine;
 public class Minion : MonoBehaviour
 {
     public GameObject Jugador;
-    private NavMeshAgent agenteNavegacion;
+    public Transform Casa;
+    public LayerMask mascaraJugador;
+    public float RadioVision = 15;
+    NavMeshAgent agenteNavegacion;
+
+    Collider colisionador;
     // Start is called before the first frame update
     void Start()
     {
         this.agenteNavegacion = GetComponent<NavMeshAgent>();
-        
+
         agenteNavegacion.destination = Jugador.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        agenteNavegacion.destination = Jugador.transform.position;
+        Debug.Log(ComprobarRadio());
+        if (ComprobarRadio())
+        {
+            this.agenteNavegacion.destination = Jugador.transform.position;
+        }
+        else
+        {
+            this.agenteNavegacion.destination = Casa.position;
+        }
     }
 
+    private bool ComprobarRadio()
+    {
+        RaycastHit[] JugadoresEnObjetivo = Physics.SphereCastAll(this.transform.position, RadioVision, this.transform.forward, RadioVision, mascaraJugador);
+        if (JugadoresEnObjetivo.Length > 0)
+        {
+
+            return true;
+        }
+        else
+        {
+
+            return false;
+        }
+    }
 }
